d6c39e87467505a51b3a31ea0f109f4cffe8fbdc
Timothy Moore <tjstretchalot@gmail.com>
2014-07-30 10:48:36 -0700

    Adapter over listener

--- src/me/timothy/twittertoreddit/TwitterToReddit.java
+++ src/me/timothy/twittertoreddit/TwitterToReddit.java
@@ -22,15 +22,9 @@ import java.util.regex.Pattern;
 
 import org.json.simple.parser.ParseException;
 
-import twitter4j.DirectMessage;
-import twitter4j.StallWarning;
 import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
-import twitter4j.User;
-import twitter4j.UserList;
+import twitter4j.UserStreamAdapter;
 import twitter4j.UserStreamListener;
 
 import com.github.jreddit.utils.restclient.HttpRestClient;
@@ -49,17 +43,7 @@ public class TwitterToReddit {
 		this.twitterId = twId;
 		this.subreddit = sub;
 		
-		listener = new UserStreamListener() {
-
-			@Override
-			public void onDeletionNotice(StatusDeletionNotice arg0) {}
-
-			@Override
-			public void onScrubGeo(long arg0, long arg1) {}
-
-			@Override
-			public void onStallWarning(StallWarning arg0) {}
-
+		listener = new UserStreamAdapter() { 
 			@Override
 			public void onStatus(final Status status) {
 				new Thread(new Runnable() {
@@ -71,67 +55,6 @@ public class TwitterToReddit {
 					
 				}).start();
 			}
-
-			@Override
-			public void onTrackLimitationNotice(int arg0) {}
-
-			@Override
-			public void onException(Exception arg0) {}
-
-			@Override
-			public void onBlock(User arg0, User arg1) {}
-
-			@Override
-			public void onDeletionNotice(long arg0, long arg1) {}
-
-			@Override
-			public void onDirectMessage(DirectMessage arg0) {}
-
-			@Override
-			public void onFavorite(User arg0, User arg1, Status arg2) {}
-
-			@Override
-			public void onFollow(User arg0, User arg1) {}
-
-			@Override
-			public void onFriendList(long[] arg0) {}
-
-			@Override
-			public void onUnblock(User arg0, User arg1) {}
-
-			@Override
-			public void onUnfavorite(User arg0, User arg1, Status arg2) {}
-
-			@Override
-			public void onUnfollow(User arg0, User arg1) {}
-
-			@Override
-			public void onUserListCreation(User arg0, UserList arg1) {}
-
-			@Override
-			public void onUserListDeletion(User arg0, UserList arg1) {}
-
-			@Override
-			public void onUserListMemberAddition(User arg0, User arg1,
-					UserList arg2) {}
-
-			@Override
-			public void onUserListMemberDeletion(User arg0, User arg1,
-					UserList arg2) {}
-
-			@Override
-			public void onUserListSubscription(User arg0, User arg1,
-					UserList arg2) {}
-
-			@Override
-			public void onUserListUnsubscription(User arg0, User arg1,
-					UserList arg2) {}
-
-			@Override
-			public void onUserListUpdate(User arg0, UserList arg1) {}
-
-			@Override
-			public void onUserProfileUpdate(User arg0) {}
 			
 		};
 	}
